30242e87e8306b2ff8ca1eee381c4ff233d9bc8e
sipols <>
2019-08-27 14:50:07 +0200

    JS and microsite

---
+++ JS/scriptInfo.js
@@ -0,0 +1,7 @@
+var myName = "Sipols";
+var myAge = 105;
+var myCity = "Hamburg";
+var info = "Hey, I am " + myName + " and I am " + myAge + " years old. "
+ + "My city is " + myCity + ". ";
+
+ console.log(info);
