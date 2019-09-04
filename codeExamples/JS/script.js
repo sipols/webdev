a3dcfc26f5e4d23cdcf17cdb76e4e0d4eb9f3104
sipols <>
2019-09-04 22:23:38 +0200

    two nigel added

---
+++ codeExamples/JS/script.js
@@ -0,0 +1,34 @@
+// executed first before other js
+// alert("external JS file");
+// alert("Hey I cam from external JS file");
+// alert("the outcome of 2+6*10%2: " + 2+6*10%2);
+console.log("Hey there I am in your browser in console tab");
+console.log(558+566);
+
+var x=10;
+x=x+x;
+var y=5;
+y=x;
+var h=9.6; //double
+var binary=0b101001; //how to define a number in binary
+var s="Sehr geehrte Damen und Herren ";
+var hexNum=0xe4; //hex numbers
+var hexNum2=0xFF;
+var str="15665486435165684351181865562484";
+var CAP=5;
+var cap=10; //sensitive for CAPS
+var caP=15;
+var avr=(CAP/cap)/3;
+
+
+console.log("X is:" + x);
+console.log(y+x);
+console.log(s+x);
+console.log("Binary 101001  is:"+binary);
+console.log("Hex" + hexNum);
+console.log("Hex" + hexNum2);
+console.log(str);
+console.log(avr);
+
+// do not use keywords of JS for variables var like
+// class const var +-
